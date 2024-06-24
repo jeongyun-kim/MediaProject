11ced7b7eece3d077c59ef2759046e90529059e3
jeongyun-kim <kkanyo04@naver.com>
2024-06-24 18:50:51 +0900

    RecommendVC에서 보여줄 각 컬렉션뷰의 타이틀 Case로 정의 / 검색결과, 추천영화, 비슷한 영화가 없을 때 보여줄 EmptyView 내 Label을 위한 Case 정의

--- MediaProject/Constant/Enums.swift
+++ MediaProject/Constant/Enums.swift
@@ -18,7 +18,6 @@ enum ButtonImage: String {
     case more = "ellipsis"
 }
 
-
 enum LabelText: String {
     case gradeLabel = "평점"
     case moreLabel = "자세히 보기"
@@ -30,4 +29,16 @@ enum LabelText: String {
 enum DetailSectionTitles: String, CaseIterable {
     case overview = "줄거리"
     case casting = "캐스팅"
+}
+
+enum CollectionViewTitle: String {
+    case similar = "비슷한 영화"
+    case recommend = "추천 영화"
+    case posters = "포스터"
+}
+
+enum CollectionViewType: String {
+    case search = "검색결과가 없습니다 :( "
+    case recommend = "관련 추천 영화가 없습니다 X( "
+    case similar = "비슷한 영화가 없어요 T^T"
 }
